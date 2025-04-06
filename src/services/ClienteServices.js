ff39b5515bdcc1b1aeb9a27b36dd183c3c5c165b
Michelle839 <michellealexandrard@ufps.edu.co>
2025-04-05 23:23:09 -0500

    modificaciones en el login

--- src/services/ClienteServices.js
+++ src/services/ClienteServices.js
@@ -1,14 +1,13 @@
 import cliente from "../models/Cliente.js";
 
-
 export async function listar() {
-    return await cliente.findAll();
-};
+  return await cliente.findAll();
+}
 
 export async function buscarPorCedula(dni) {
-    return await cliente.findByPk(dni);
-};
+  return await cliente.findByPk(dni);
+}
 
 export async function registrarCliente(datosCliente) {
-    return await cliente.create(datosCliente);
-};
+  return await cliente.create(datosCliente);
+}
