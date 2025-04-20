d075b8ab282bb25cb4775eb5cc28d2d3b6624cee
Michelle839 <michellealexandrard@ufps.edu.co>
2025-04-19 19:41:14 -0500

    funcionalidad de actualizar membresias

--- src/routes/MembresiaRoutes.js
+++ src/routes/MembresiaRoutes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { listar, crear, buscarPorId } from "../controllers/MembresiaController.js";
+import {
+  listar,
+  crear,
+  buscarPorId,
+  actualizar,
+} from "../controllers/MembresiaController.js";
 
 const router = Router();
 
 router.get("/", listar);
 router.post("/", crear);
+router.put("/:id_membresia", actualizar);
 router.get("/:id_m", buscarPorId);
 
 export default router;
